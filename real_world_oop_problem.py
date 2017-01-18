e5251b7f08118fbef9bd1e3ef4d78720ea63be55
brendenozie <brendenodhiambo@gmail.com>
2017-01-18 08:07:07 +0300

    second commit

--- real_world_oop_problem.py
+++ real_world_oop_problem.py
@@ -1,3 +1,5 @@
+#Registration problem solved using OOP
+
 class person:
     def __init__(self,name,age,DoB,address,contact,gender):
         self.name=name
@@ -6,6 +8,11 @@ class person:
         self.address=address
         self.contact=contact
         self.gender=gender
+    def __str__(self):
+        return "Title:%s , author:%s, pages:%s " (self.title, self.author, self.pages)
+    def walk():
+        return 'i am walking "
+              
 class employee(person):
     def __init__(self,occupation,workDepartment,job,salary):
         self.occupation=occupation
@@ -17,18 +24,25 @@ class teacher(employee):
         self.subject=subject
         self.teachingClass=teachingClass
     def teach():
-        print 'i am teaching'
+        return 'i am teaching'
+    def walk:
+        return 'i am walking to the staffroom'
 
 class cook(employee):
     def __init__(self,position):
         self.position=position
     def cook():
-        print 'i am cookin'
+        return 'i am cookin'
+    def walk():
+        return 'i am walking to the kitchen'
+    
 class student(person):
     def __init__(self,parentName,parentNumber,parentContact):
         self.parentName=parentName
         self.parentNumber=parentNumber
         self.parentContact=parentContact
+    def walk():
+        return 'i am walking to class'
         
         
         
